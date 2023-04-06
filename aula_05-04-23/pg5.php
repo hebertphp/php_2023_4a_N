5da6a98345fd8c46cadce96449ffea45bb3fdf18
Prof. Hebert <hebertphp@gmail.com>
2023-04-05 21:56:57 -0300

    05/04/23

--- aula_05-04-23/pg5.php
+++ aula_05-04-23/pg5.php
@@ -2,7 +2,11 @@
 $a=50;
 $b=120;
 $c=200;
-$d=($a<=$b) ? "Verdadeiro" : "Falso";
-$e=($a>=$c) ? "Verdadeiro" : "Falso";
+$d=($a<=$b) ? "Blz" : "acho que não";
+$e=($a>=$c) ? "OK" : "Zoado";
 echo "d = ".$d."<br>e = ".$e;
+/*
+Oper. Ternário
+(condição) ? valor, condição verdadeira : valor, condição falsa
+*/
 ?>
