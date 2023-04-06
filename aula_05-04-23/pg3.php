286c123350d891e7aca3bf6387e03e4f7e4e764e
Prof. Hebert <hebertphp@gmail.com>
2023-04-05 21:18:42 -0300

    05/04/23

--- aula_05-04-23/pg3.php
+++ aula_05-04-23/pg3.php
@@ -1,6 +1,14 @@
 <?php
 $a = 10;
 $b = "10";
-$c = $a==$b;
+$c = $a!==$b;
 var_dump($c);
+/*
+=   atribuir/receber
+==  comparação de igualdade verifica apenas o valor
+=== comparação para idêntico verifica o valor, depois o tipo
+
+!=  não igual
+!== não idêntico
+*/
 ?>
