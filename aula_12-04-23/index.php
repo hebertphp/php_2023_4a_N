48ef480b501b535b14fcab9c512cf5f95e26d7cc
Prof. Hebert <hebertphp@gmail.com>
2023-04-12 20:15:18 -0300

    12/04/23

---
+++ aula_12-04-23/index.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+CREATE TABLE ALUNO(
+    Matric number(4) CONSTRAINT ALUNO_Matric_PK PRIMARY KEY,
+    Nome VARCHAR2(50) NOT NULL,
+    Telefone varchar2(20),
+    endereco varchar2(80),
+    UF char(2)
+);
+*/
+?><hr>
+<a href="form.html">form</a><br>
