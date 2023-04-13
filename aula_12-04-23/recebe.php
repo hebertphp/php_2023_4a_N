0555f443b0485e7323ab62a081eab5a1a742d9e4
Prof. Hebert <hebertphp@gmail.com>
2023-04-12 21:42:05 -0300

    12/04/23

--- aula_12-04-23/recebe.php
+++ aula_12-04-23/recebe.php
@@ -3,7 +3,12 @@ echo "<h1>Receber os dados</h1>";
 
 $matric=$_POST["matric"];
 $nome=$_POST["nome"];
+$telefone=$_POST["telefone"];
+$endereco=$_POST["endereco"];
+$uf=$_POST["uf"];
 
 echo "Matrícula: $matric<br>Nome: $nome<br>";
+echo "Telefone: $telefone<br>Endereço: $endereco<br>";
+echo "UF: $uf";
 ?><hr>
 <a href="form.html">form</a><br>
