be8a13666a7095125ad828d2f7710502dd692800
Prof. Hebert <hebertphp@gmail.com>
2023-04-19 20:45:55 -0300

    19/04/23

--- aula_19-04-23/par_impar.php
+++ aula_19-04-23/par_impar.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Par-Ímpar</title>
+</head>
+<body>
+    <h1>Par-Ímpar</h1>
+    <form method="post" action="par_impar.php">
+    Digite um número<input type="number" name="numero">
+    <input type="submit" value="Enviar!!">
+    </form>
+    
+</body>
+</html>
+
 <?php
 /*
 Elaborar um código PHP para dado um número inteiro, 
@@ -7,9 +25,6 @@ como mostrado a seguir:
 
 275 - ÍMPAR
 28 - PAR
-
 */
 $numero=275;
-
-
 ?>
