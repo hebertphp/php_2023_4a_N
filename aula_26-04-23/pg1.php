d3f8ccfc6a48068711494400850c785f55fa0ba6
Prof. Hebert <hebertphp@gmail.com>
2023-04-26 20:46:20 -0300

    26/04/23

--- aula_26-04-23/pg1.php
+++ aula_26-04-23/pg1.php
@@ -1,16 +1,17 @@
 <?php
-$destino="New York";
+$destino="New York";//Nova York//NYork//NY
 switch ($destino) {
-    case "São Paulo":
+    case "São Paulo"://SP //s paulo
         echo 'Seja bem vindo a cidade que nunca para!';
         break;
     case "New York":
-        echo 'Seja bem vindo a Big Apple!';
+        echo "Seja bem vindo a Big Apple!";
         break;
     case "Paris":
         echo 'Seja bem vindo a cidade Luz!';
         break;
-    default:
+    default://else
         echo 'Não encontramos seu destino!';
+        break;
 }
 ?>
---
+++ aula_26-04-23/pg2.php
@@ -0,0 +1,15 @@
+<?php
+$i=0;
+while ($i <= 5) {
+    echo  "$i  ";
+    $i++;
+}
+/*
+Saída:
+1) Qual o valor inicial?
+2) Qual a condição?
+3) Qual é o contador?
+4) Quantas vezes o looping foi executado?
+5) Qual o valor que tornou o looping falso?
+*/
+?>
