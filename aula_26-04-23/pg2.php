e54b1c16ddbde9fbbe91156200d1baf4e18d5800
Prof. Hebert <hebertphp@gmail.com>
2023-04-26 20:48:08 -0300

    26/04/23

--- aula_26-04-23/pg2.php
+++ aula_26-04-23/pg2.php
@@ -10,6 +10,6 @@ Saída:
 2) Qual a condição?
 3) Qual é o contador?
 4) Quantas vezes o looping foi executado?
-5) Qual o valor que tornou o looping falso?
+5) Qual o valor que tornou a condição falso?
 */
 ?>
