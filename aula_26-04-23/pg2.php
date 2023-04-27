efadcc33afd026034a694ef062f5455c827a614b
Prof. Hebert <hebertphp@gmail.com>
2023-04-26 21:03:38 -0300

    26/04/23

--- aula_26-04-23/pg2.php
+++ aula_26-04-23/pg2.php
@@ -4,12 +4,22 @@ while ($i <= 5) {
     echo  "$i  ";
     $i++;
 }
+echo "<hr>";
 /*
-Saída:
-1) Qual o valor inicial?
-2) Qual a condição?
-3) Qual é o contador?
-4) Quantas vezes o looping foi executado?
-5) Qual o valor que tornou a condição falso?
+Saída: 0 1 2 3 4 5 
+1) Qual o valor inicial? 0
+2) Qual a condição? $i<=5
+3) Qual é o contador? $i++
+4) Quantas vezes o looping foi executado? 6
+5) Qual o valor que tornou a condição falso? 6
+*/
+
+/*
+Saída: 7 8 9 10
+1) Qual o valor inicial? 
+2) Qual a condição? 
+3) Qual é o contador? 
+4) Quantas vezes o looping foi executado? 
+5) Qual o valor que tornou a condição falso? 
 */
 ?>
