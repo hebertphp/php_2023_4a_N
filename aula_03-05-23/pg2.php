6c328e5a7c5a52aa9638a07ca9479ef4d38f44b7
Prof. Hebert <hebertphp@gmail.com>
2023-05-03 21:22:50 -0300

    03/05/23

--- aula_03-05-23/pg2.php
+++ aula_03-05-23/pg2.php
@@ -1,7 +1,17 @@
 <?php
-for ($i=0; $i<4; $i++) {
-    for ($j=0; $j<5; $j++) {
+for ($i=2; $i<4; $i++) {//externo//minuto
+    for ($j=3; $j<=5; $j++) {//interno//segundo
         echo "$i $j<br>";
     }
 }
+/*
+$i  $j
+2   3
+2   4
+2   5
+3   3
+3   4
+3   5
+
+*/
 ?>
---
+++ aula_03-05-23/pg3.php
@@ -0,0 +1,8 @@
+<?php
+$num[0]=10;
+$num[1]=30;
+$num[2]=2023;
+echo $num[0]."<br>";
+echo $num[1]."<br>";
+echo $num[2]."<br>";
+?>
