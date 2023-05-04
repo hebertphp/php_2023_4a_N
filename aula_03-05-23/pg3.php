2d7960fcdaf1fc68f6f455b3e6170b9583e82cf4
Prof. Hebert <hebertphp@gmail.com>
2023-05-03 21:36:56 -0300

    03/05/23

--- aula_03-05-23/pg3.php
+++ aula_03-05-23/pg3.php
@@ -5,4 +5,15 @@ $num[2]=2023;
 echo $num[0]."<br>";
 echo $num[1]."<br>";
 echo $num[2]."<br>";
+echo "<hr>";
+//         0    1    2    3            
+$uf=array("SP","RJ","ES","MG");
+echo $uf[1];
+echo "<hr>";
+$dados=array(10,"oi",7.5);
+echo $dados[0]."<br>";
+echo $dados[1]."<br>";
+echo $dados[2]."<hr>";
+$dados=array(1,2,3,10,30,90);
+echo "<pre>";print_r($dados);echo "</pre>";
 ?>
