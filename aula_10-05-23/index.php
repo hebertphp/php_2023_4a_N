c4c5e67bda945858a9e4546569350d7413caeab2
Prof. Hebert <hebertphp@gmail.com>
2023-05-10 20:25:54 -0300

    10/05/23

---
+++ aula_10-05-23/index.php
@@ -0,0 +1,6 @@
+<?php
+echo "PHP - 10/05/23";
+?><hr>
+<a href="pg1.php">pg1</a><br>
+<a href="pg2.php">pg2</a><br>
+<a href="pg3.php">pg3</a><br>
---
+++ aula_10-05-23/pg1.php
@@ -0,0 +1,9 @@
+<?php
+$u="SP";
+$u="RJ";
+//         0    1    2    3    4 
+$uf=array("SP","RJ","TO","SC","AM");
+echo $uf[1];
+echo "<hr>";
+
+?>
