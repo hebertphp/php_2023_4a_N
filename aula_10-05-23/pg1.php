ef79649f0f99d86340f4ddc1ef026e580f3a898d
Prof. Hebert <hebertphp@gmail.com>
2023-05-10 21:11:52 -0300

    10/05/23

--- aula_10-05-23/pg1.php
+++ aula_10-05-23/pg1.php
@@ -17,4 +17,16 @@ $cli["horas"]=120;
 $cli["valor_hora"]=15.5;
 echo "Cpf: ".$cli["cpf"];
 echo "<pre>";print_r($cli);echo "</pre>";
+echo "<hr>";
+$aluno = array(
+            array("TBD","Fulano",123456789),
+            array("TRC","Beltrano",234567890),
+            array("TADS","Sicrano",345678901) 
+            );
+      //     0                   1                                          2  3
+$arr=array(array(1,2,3,4,"oi"),array("João","Andressa","Pablo","Vinicius"),30,40);
+//               0 1 2 3              0       1            2       3
+echo $arr[1][1];echo "<hr>";
+echo "<pre>";print_r($arr);echo "</pre>";
+   
 ?>
