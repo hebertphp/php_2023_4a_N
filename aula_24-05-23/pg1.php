0ab4a7f3bea2e5472e61a1c3cc09b5a95eb6af70
Prof. Hebert <hebertphp@gmail.com>
2023-05-24 20:49:04 -0300

    24/05/23

--- aula_24-05-23/pg1.php
+++ aula_24-05-23/pg1.php
@@ -1,6 +1,6 @@
 <?php
 $uf = array("SP","RJ","MG","ES");
 foreach ($uf as $valor){
-echo $valor . "<br>";
+    echo $valor . "<br>";
 }
 ?>
---
+++ aula_24-05-23/pg2.php
@@ -0,0 +1,14 @@
+<?php
+//            0  1  2  3  4
+$numero=array(10,20,30,40,50);
+$x=$numero[0];
+echo "$x<br>";
+$x=$numero[1];
+echo "$x<br>";
+$x=$numero[2];
+echo "$x<br>";
+$x=$numero[3];
+echo "$x<br>";
+$x=$numero[4];
+echo "$x<br>";
+?>
