d972085a0b0df8166c655a184974a772197fb20c
Prof. Hebert <hebertphp@gmail.com>
2023-05-24 21:16:12 -0300

    24/05/23

--- aula_24-05-23/pg3.php
+++ aula_24-05-23/pg3.php
@@ -2,10 +2,12 @@
 $estudante=array(
     "ra"   =>123456,
     "nome" =>"Jubileu",
-    "curso"=>"TADS"
+    "curso"=>"TADS",
+    "semestre"=>3
 );
 echo "<pre>";print_r($estudante);echo "</pre><hr>";
 foreach($estudante as $pos=>$valor){
-    echo "$pos: <b>$valor</b><br>";
+    //echo "$pos: <b>$valor</b><br>";
+    echo "$valor<br>";
 }
 ?>
---
+++ aula_24-05-23/pg4.php
@@ -0,0 +1,8 @@
+<?php
+$uf=array("SP","RJ","MG","ES","RS","MT","TO","AM");
+sort($uf);
+foreach($uf as $x){
+    echo "$x<br>";
+}
+
+?>
