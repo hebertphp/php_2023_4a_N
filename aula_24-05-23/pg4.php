1a938b78d1b28b670a925119a1b4d8dc0f454c19
Prof. Hebert <hebertphp@gmail.com>
2023-05-24 21:19:50 -0300

    24/05/23

--- aula_24-05-23/pg4.php
+++ aula_24-05-23/pg4.php
@@ -1,8 +1,10 @@
+UF<select name="uf">
+
 <?php
 $uf=array("SP","RJ","MG","ES","RS","MT","TO","AM");
 sort($uf);
 foreach($uf as $x){
-    echo "$x<br>";
+    echo "<option value='SP'>SP</option>";
 }
-
 ?>
+</select><br>
