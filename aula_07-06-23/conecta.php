98eebc712bac6e6a189cfa74a92237fac321f485
Prof. Hebert <hebertphp@gmail.com>
2023-06-07 20:51:10 -0300

    07/06/23

--- aula_07-06-23/conecta.php
+++ aula_07-06-23/conecta.php
@@ -1,16 +1,17 @@
 <?php
-define("HOST","localhost:3306");
+define("HOST","localhost");
 define("USER","root");
 define("PASS","");
-define("DB","test");
+define("DB","testes");
+
 error_reporting(0);
 mysqli_report(MYSQLI_REPORT_OFF);
 $conn = mysqli_connect(HOST, USER, PASS, DB);
 if (!$conn) {
-  die("Falha ao conectar com o servidor: " . mysqli_connect_error());
+    die("Falha ao conectar com o servidor: " . mysqli_connect_error());
 }
 else {
-  echo "Conexão efetuada com sucesso!";
+    echo "Conexão efetuada com sucesso!";
 }
 //mysqli_close($conn);
 ?>
