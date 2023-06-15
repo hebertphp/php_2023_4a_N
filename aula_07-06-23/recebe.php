946c4df4fda1a14d7ae55aa8ca5422f83f3f9730
Prof. Hebert <hebertphp@gmail.com>
2023-06-14 22:00:05 -0300

    14/06/23

--- aula_07-06-23/recebe.php
+++ aula_07-06-23/recebe.php
@@ -1,11 +1,16 @@
 <?php
 include "conecta.php";
 //echo "<pre>";print_r($_POST);
+if(count($_POST)==0){
+    echo "Formulário primeiro";
+    exit();
+}
 $login=$_POST["login"];
 $senha=$_POST["senha"];//
 $email=$_POST["email"];
+$hashed_password = password_hash($senha, PASSWORD_DEFAULT);
 $sql="
-insert into user values (null,'$login', md5('$senha'), '$email', now(), null)
+insert into user values (null,'$login', '$hashed_password', '$email', now(), null)
 ";
 //echo $sql;
 $query = mysqli_query($conn,$sql) or die (mysqli_error($conn));
