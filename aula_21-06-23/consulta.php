4a9ad2b21b88e57885f6dbb9784eb80265b7d864
Prof. Hebert <hebertphp@gmail.com>
2023-06-21 20:55:16 -0300

    21/06/23

--- aula_21-06-23/consulta.php
+++ aula_21-06-23/consulta.php
@@ -1,11 +1,19 @@
 <?php
 include "conecta.php";
-$sql="select login, email, data from usuario";
+$sql="select login, email, data from user";
 $result = mysqli_query($conn,$sql) or die (mysqli_error($conn));
 $row= mysqli_fetch_array($result, MYSQLI_ASSOC);
 echo "<pre>";print_r($row);echo "</pre>";
 $row= mysqli_fetch_array($result, MYSQLI_ASSOC);
 echo "<pre>";print_r($row);echo "</pre>";
+?>
+<table border="1" width="500" align="center">
+    <tr>
+        <th>Login</th><th>email</th><th>data</th>
+    </tr>
+
+</table>
+<?php
 mysqli_free_result($result);
 mysqli_close($conn);
 ?>
