28ad63e1f7699c15e6e59384f166e2c18010302e
Prof. Hebert <hebertphp@gmail.com>
2023-06-21 21:14:17 -0300

    21/06/23

--- aula_21-06-23/consulta.php
+++ aula_21-06-23/consulta.php
@@ -2,18 +2,27 @@
 include "conecta.php";
 $sql="select login, email, data from user";
 $result = mysqli_query($conn,$sql) or die (mysqli_error($conn));
-$row= mysqli_fetch_array($result, MYSQLI_ASSOC);
+//$row= mysqli_fetch_array($result, MYSQLI_ASSOC);
 echo "<pre>";print_r($row);echo "</pre>";
-$row= mysqli_fetch_array($result, MYSQLI_ASSOC);
+//$row= mysqli_fetch_array($result, MYSQLI_ASSOC);
+//$row= mysqli_fetch_array($result, MYSQLI_ASSOC);
 echo "<pre>";print_r($row);echo "</pre>";
 ?>
 <table border="1" width="500" align="center">
     <tr>
         <th>Login</th><th>email</th><th>data</th>
     </tr>
-
+    <?php
+    while($row=mysqli_fetch_array($result, MYSQLI_ASSOC)){
+        echo "
+        <tr>
+            <td>".$row["login"]."</td><td>".$row["email"]."</td><td>".$row["data"]."</td>
+        </tr> ";
+    }
+    ?>
 </table>
 <?php
+echo $row["data"];
 mysqli_free_result($result);
 mysqli_close($conn);
 ?>
